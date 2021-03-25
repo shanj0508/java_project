de9c5453c9daef1ad1946906099e4d8eeb9a51a8
shanjing <605284854@qq.com>
2021-03-25 17:59:10 +0800

    TODO

--- src/TestPackage/Demo_sortHalfSearch.java
+++ src/TestPackage/Demo_sortHalfSearch.java
@@ -3,12 +3,15 @@ package TestPackage;
 public class Demo_sortHalfSearch {
     public static void main(String[] args) {
         int[] arr = {1, 3, 10, 12, 19, 27, 33, 40, 52, 68, 79, 88, 91, 100};
+        //递归实现
         System.out.println(halfSearch(arr, 0, arr.length - 1, 52));
+        //常规方式
+        System.out.println(find(arr,9));
 
     }
 
     /**
-     * 二分查找-递归实现
+     * 二分查找,返回索引-递归实现
      */
     public static int halfSearch(int[] arr, int low, int hight, int target) {
         if (low <= hight) {
@@ -23,5 +26,28 @@ public class Demo_sortHalfSearch {
         } else {
             return -1;
         }
+    }
+    /**
+     * TODO:二分查找,返回索引-常规实现
+     */
+
+    private static int find(int [] arr,int searchKey){
+        int lowerBound = 0;
+        int upperBound = arr.length -1;
+        int curIn;
+        while(lowerBound <= upperBound){
+            curIn = (lowerBound + upperBound) / 2;
+            if(arr[curIn] == searchKey){
+                return curIn;
+            }else{
+                if(arr[curIn] < searchKey){
+                    lowerBound = curIn + 1;
+                }else{
+                    upperBound = curIn - 1;
+                }
+            }
+        }
+        return -1;
+
     }
 }
