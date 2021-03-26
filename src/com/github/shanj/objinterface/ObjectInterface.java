63df21dfe5a6c6db0a337631c5d3bbf349c9ab7c
shanjing <605284854@qq.com>
2021-03-26 09:53:52 +0800

    重构文件目录

--- src/com/github/shanj/objinterface/ObjectInterface.java
+++ src/com/github/shanj/objinterface/ObjectInterface.java
@@ -1,8 +1,8 @@
-package com.github.shanj.obinterface;
+package com.github.shanj.objinterface;
 
 /**
  * 用接口给一个有工资收入和稿费收入的小伙伴算税。
  */
 //TODO
-public class DemoObjectInterface {
+public class ObjectInterface {
 }
